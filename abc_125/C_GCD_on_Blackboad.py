617b2d1f7746f576692f7eaf23b17a99f7077fcb
yu-ya4 <yuya4.matsumura@gmail.com>
2019-04-28 01:22:12 +0900

    GCD for L_i and R_i...

--- abc_125/C_GCD_on_Blackboad.py
+++ abc_125/C_GCD_on_Blackboad.py
@@ -8,15 +8,27 @@ def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
-n = 0
 
-if N == 2:
-    n = max(A)
-else:
-    for i in range(N):
-        l = copy.copy(A)
-        l.pop(i)
-        res = functools.reduce(lambda x, y: gcd(x, y), l)
-        if res > n:
-            n = res
-print(n)
+def gcd_l(l):
+    return functools.reduce(lambda x, y: gcd(x, y), l)
+
+
+l = [0] * N  #L_i, i = 0, 1, 2, ..., N-1
+for i in range(N):
+    if i == 0:
+        continue
+    else:
+        l[i] = gcd(l[i-1], A[i-1])
+
+r = [0] * N #R_i, i = 0, 1, 2, ..., N-1
+for j in range(N):
+    if j == 0:
+        continue
+    else:
+        r[N-j] = gcd(r[N-j], A[N-j-1])
+
+m = [0] * N
+for i in range(N):
+    m[i] = gcd(l[i], r[i])
+
+print(max(m))
